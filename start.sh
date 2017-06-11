c25f946ea955fd3bda68779e785705aee5a135d0
Soichi Hayashi <hayashis@iu.edu>
2017-06-11 08:53:17 -0400

    removed SCA_ references

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#mainly to debug locally
-if [ -z $SCA_WORKFLOW_DIR ]; then export SCA_WORKFLOW_DIR=`pwd`; fi
-if [ -z $SCA_TASK_DIR ]; then export SCA_TASK_DIR=`pwd`; fi
-if [ -z $SCA_SERVICE_DIR ]; then export SCA_SERVICE_DIR=`pwd`; fi
+#needed to run locally
+if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 
 #clean up previous job (just in case)
 rm -f finished
-jobid=`qsub $SCA_SERVICE_DIR/submit.pbs`
+jobid=`qsub $SERVICE_DIR/submit.pbs`
 echo $jobid > jobid
 
